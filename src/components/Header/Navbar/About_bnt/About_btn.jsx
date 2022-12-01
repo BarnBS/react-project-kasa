65d2a5c16d464a0313c4b522e7a80d4b71a7434c
Barnabé Samel <barnabe.samel@gmail.com>
2022-12-01 15:53:51 +0100

    create CSS for header's components

---
+++ src/components/Header/Navbar/About_bnt/About_btn.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+ 
+function About() {
+    return (
+        <nav>
+            <Link to="/about">A Propos</Link>
+        </nav>
+    )
+}
+
+export default About
---
+++ src/components/Header/Navbar/Home_btn/Home_btn.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+ 
+function Home() {
+    return (
+        <nav>
+            <Link to="/">Accueil</Link>
+        </nav>
+    )
+}
+
+export default Home
