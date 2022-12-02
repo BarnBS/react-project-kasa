9ee4fd976ccb57e3fd8dff6f7f93f94d26358dc6
Barnabé Samel <barnabe.samel@gmail.com>
2022-12-02 15:55:46 +0100

    finish error404 page layout and style

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
+import "./Error404.css"
 
 function Error404 () {
 
     return <>
-        <h1>ERROR</h1>
+    <section className="Error404">
+        <h1>404</h1>
+        <p> Oups ! La page que </p>
+        <p> vous demandez n'existe pas.</p>
+        <Link to="/">Retourner sur la page d'accueil</Link>
+    </section>
     </>
 }
 
