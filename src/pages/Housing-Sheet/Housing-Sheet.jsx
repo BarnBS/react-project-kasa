79aa7bcc1779fa2e60f377d2de2437e8a539ab7c
Barnabé Samel <barnabe.samel@gmail.com>
2022-12-09 11:25:57 +0100

    check url id param to display the right housing page, else error404

--- src/pages/Housing-Sheet/Housing-Sheet.jsx
+++ src/pages/Housing-Sheet/Housing-Sheet.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import Error404 from "../Error404/Error404";
+import logements from "../../logements.json"
 
 function Housing_Sheet () {
 
-    return <>
-        <h1>HOUSING</h1>
-    </>
+    const logementsList = Array.from(logements.values());
+    const urlList = []
+    for (let i=0; i<logementsList.length; i++) {
+        urlList.push(logements[i].id);
+    }
+
+    let urlParameter = useParams();
+    
+    for (let i=0; i < urlList[i].length; i++) {
+        if (urlList.find(idFromList => idFromList === urlParameter.id)) {
+
+            return <>
+                
+            </>
+
+        }
+        
+        return <>
+            <Error404/>
+        </>
+    }
 }
+
 
 export default Housing_Sheet
