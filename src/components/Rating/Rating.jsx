2b0576c0250de3e6a4b839a4a207e3c03e0de29c
Barnabé Samel <barnabe.samel@gmail.com>
2022-12-19 15:30:41 +0100

    refactor rating component

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -2,44 +2,44 @@ import star from "./star.png"
 
 import "./Rating.css"
 
+//Count the number of colored stars and add to a list each star to iterate on it later
+function coloredStars(prop){
+    let coloredStarsList = [];
+    let stars = Number(prop);
+    let star = "colored Star"
+    while (stars > 0) {
+        coloredStarsList.push(star);
+        stars--;
+    }
+    return coloredStarsList;
+}
+    
+//Count the number of colored stars and add to a list each star to iterate on it later
+function notColoredStars(prop){
+    let notColoredStarsList = [];
+    let stars = Number(prop);
+    let noStar = "not Colored Star";
+    while (stars < 5) {
+        notColoredStarsList.push(noStar);
+        stars++;
+    }
+    return notColoredStarsList;
+}
+
 //Returns the rating stars/5
 function Rating(props){
     const {rating} = props;
-    let coloredStarsList = [];
-    let notColoredStarsList = [];
-
-    //Count the number of colored stars and add to a list each star to iterate on it later
-    function coloredStars(){
-        let stars = Number(rating);
-        let i = "colored Star"
-        while (stars > 0) {
-            coloredStarsList.push(i);
-            stars--;
-        }
-        return coloredStarsList;
-    }
-    
-    //Count the number of colored stars and add to a list each star to iterate on it later
-    function notColoredStars(){
-        let stars = Number(rating);
-        let i = "not Colored Star";
-        while (stars < 5) {
-            notColoredStarsList.push(i);
-            stars++;
-        }
-        return notColoredStarsList;
-    }
 
     return <div className="rating">
         {   
-            coloredStars().map( coloredStar => {
-                return <img key={coloredStars().indexOf(coloredStar)}  src={star} alt="star used for rating" className="star"/>
+            coloredStars(rating).map( coloredStar => {
+                return <img key={coloredStars(rating).indexOf(coloredStar)}  src={star} alt="star used for rating" className="star"/>
             })
         }
 
         {
-            notColoredStars().map( notColoredStar => {
-                return <img key={notColoredStars().indexOf(notColoredStar) + Math.random}  src={star} alt="star used for rating" className="star uncoloredStar"/>
+            notColoredStars(rating).map( notColoredStar => {
+                return <img key={notColoredStars(rating).indexOf(notColoredStar) + Math.random}  src={star} alt="star used for rating" className="star uncoloredStar"/>
             })
         }
     </div>
