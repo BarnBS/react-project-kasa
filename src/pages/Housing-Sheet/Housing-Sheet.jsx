1394b02e3567d5a5b2069fe463f5c4c98003213c
Barnabé Samel <barnabe.samel@gmail.com>
2022-12-19 15:16:18 +0100

    refactor housing-sheet

--- src/pages/Housing-Sheet/Housing-Sheet.jsx
+++ src/pages/Housing-Sheet/Housing-Sheet.jsx
@@ -13,6 +13,7 @@ import logements from "../../logements.json"
 
 import "./Housing-Sheet.css"
 
+//Returns a list of every housing id
 function listHousingIds() {
     let urlList = [];
     for (let i=0; i<logements.length; i++) {
@@ -21,21 +22,28 @@ function listHousingIds() {
     return urlList;
 }
 
+// return the housing at Integer "index" from the "logements" datas
+function housing(index) {
+    return logements[index]
+}
+
+//Returns a page-component that display every component of a housing
 function Housing_Sheet () {
 
-    let urlParameter = useParams();
+    let urlParameter = useParams(); //Get the id of the housing from the url
     
+    //If the id in the url doesn't match an id from the list of Ids then return Error404, else display the housing corresponding the id
     if (listHousingIds().find(idFromList => idFromList === urlParameter.id)) {
         for (let i=0; i < logements.length; i++) {
-            if (logements[i].id === urlParameter.id) {
+            if (housing(i).id === urlParameter.id) {
                 
             return <section className="Housing-Sheet">
 
-                <Carrousel images={logements[i].pictures}/>
-                <HousingTitle title = {logements[i].title} location = {logements[i].location}/> 
+                <Carrousel images={housing(i).pictures}/>
+                <HousingTitle title = {housing(i).title} location = {housing(i).location}/> 
                 <div className="tags">
                 {
-                    logements[i].tags.map( tag => {
+                    housing(i).tags.map( tag => {
                         return <>
                             <Tag tag={tag}/>
                         </>
@@ -43,12 +51,12 @@ function Housing_Sheet () {
                 }
                 </div>
                 <div className="host-and-ratings">
-                    <Host host = {logements[i].host.name} profile_picture = {logements[i].host.picture}/>
-                    <Rating rating = {logements[i].rating}/>
+                    <Host host = {housing(i).host.name} profile_picture = {housing(i).host.picture}/>
+                    <Rating rating = {housing(i).rating}/>
                 </div>
                 <div className="unwrap-btns">
-                    <UnwrapBtn title="Description" description={logements[i].description}/>
-                    <UnwrapBtn title="Equipements" description={logements[i].equipments.map(equipement => {
+                    <UnwrapBtn title="Description" description={housing(i).description}/>
+                    <UnwrapBtn title="Equipements" description={housing(i).equipments.map(equipement => {
                         return <>
                             <ul>
                                 {equipement}
